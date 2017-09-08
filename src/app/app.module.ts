8794efafadc07e885520bd12f40c2dd9b917918c
love1024 <lsk18mto@gmail.com>
2017-09-08 14:50:29 +0530

    Created ABC Basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreatorComponent } from './creator/creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/creator/creator.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creator',
+  templateUrl: './creator.component.html',
+  styleUrls: ['./creator.component.scss']
+})
+export class CreatorComponent implements OnInit {
+  prev = '1px';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  autoGrow(el) {
+    // Increase Height according to input
+    if (el.scrollHeight > el.clientHeight) {
+      el.style.height = el.scrollHeight + 'px';
+    } else {
+      // Prevent height from growing when deleting lines.
+      el.style.height = '1px';
+      el.style.height = el.scrollHeight + 'px';
+    }
+  }
+
+}
