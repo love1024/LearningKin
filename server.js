28ea29a3d14aa4a38d60b9ea1634598c3b7a3758
love1024 <lsk18mto@gmail.com>
2017-09-12 22:27:19 +0530

    Added compression

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // server.js
 const express = require('express');
+const compression = require('compression');
+
 const app = express();
+app.use(compression());
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
