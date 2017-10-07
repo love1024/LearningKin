e6be89e0d296aa7fb6fd23ab4f58664f3f8ecf6f
Hemant <hemant.rajput@pgi.com>
2017-10-07 12:34:01 +0530

    Added redirect to index.html for service worker

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const app = express();
 app.use(compression());
 // Run the app by serving the static files
 // in the dist directory
+
+app.get('/', function (req, res) {
+  res.redirect('https://angular-blog-creator.herokuapp.com/index.html');
+})
+
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+
 app.listen(process.env.PORT || 8080);
