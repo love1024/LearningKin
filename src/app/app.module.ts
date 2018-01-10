61c5b38d84f02397e73cfcc0227c6680aec51c6c
love1024 <lsk18mto@gmail.com>
2018-01-10 10:46:50 +0530

    Added HTTP module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CreatorComponent } from './creator/creator.component';
@@ -13,7 +13,8 @@ import { CreatorComponent } from './creator/creator.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
